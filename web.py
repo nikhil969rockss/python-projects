ebaea08e6ba818a4ded20862d140af7ecfeece47
Nikhil Randhawa <ns9013604867@gmail.com>
2026-01-23 02:24:29 +0530

    implement "complete todo" feature

--- web.py
+++ web.py
@@ -18,12 +18,16 @@ st.title("My Todo Web App")
 st.subheader("This todo app was developed by Nikhil-R")
 st.write("This is a minimal todo app to organise your day")
 
-for todo in all_todos:
-    st.checkbox(todo)
+for index,todo in enumerate(all_todos):
+    checked = st.checkbox(todo,key=todo)
+    if checked:
+        all_todos.pop(index)
+        functions.write_todos(all_todos)
+        del st.session_state[todo]
+        st.rerun()
 
 
 st.text_input(label="Enter a todo",placeholder="Enter a todo...",
               key="todo-input",label_visibility="hidden", on_change=add_todo )
 
-st.session_state
 
