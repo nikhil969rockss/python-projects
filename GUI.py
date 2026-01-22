cc667e61b9673400a23758019beb84a5937dae13
Nikhil Randhawa <ns9013604867@gmail.com>
2026-01-22 12:06:07 +0530

    add "edit" and "complete" functionality, with theme modification and "try-except" error handling

--- GUI.py
+++ GUI.py
@@ -1,20 +1,30 @@
 import FreeSimpleGUI as fsg
 import functions
 
+fsg.theme("BlueMono")
+
 input_label = fsg.Text("Type your todo in")
-input_box = fsg.InputText(tooltip="Enter todo", key="todo-input")
+input_box = fsg.InputText(tooltip="Enter todo", key="todo-input", size=(41,1))
 
 todos_box = fsg.Listbox(values=functions.get_todos(), key="todos-box",
-                        enable_events=True, size=(40,10))
+                        enable_events=True, size=(40,10), )
 
-warning_message = fsg.Text("Cannot add empty todo, Add a todo", key="warning",
-                           text_color="red", font=("Helvetica", 14), visible=False)
+add_button = fsg.Button("Add", tooltip="add-todo", size=(10,1))
 
-add_button = fsg.Button("Add")
+edit_button = fsg.Button("Edit", tooltip="edit-todo")
 
-edit_button = fsg.Button("Edit")
+complete_button = fsg.Button("Complete", tooltip="complete-todo")
 
-layout = [[input_label], [input_box, add_button], [warning_message],[todos_box, edit_button]]
+#   To make the layout column wise
+# layoutCol1 = fsg.Column([[input_label], [input_box], [todos_box]])
+# layoutCol2 = fsg.Column([[add_button], [edit_button]])
+#
+layout = [[input_label, ],
+          [input_box, add_button],
+          [todos_box, edit_button, complete_button]
+          ]
+
+
 
 window = fsg.Window("Todo-App", layout=layout, font=('Helvetica', 20) )
 
@@ -23,29 +33,23 @@ while True:
     print("event-", event)
     print("values-", values)
 
-
+    all_todos = functions.get_todos()
+    print("todos ",all_todos)
 
     match event:
         case "Add":
 
             if values['todo-input'] == "":
-                window['warning'].update(visible=True)
+                fsg.popup("Cannot add empty todo",title="Error")
                 continue
-            window['warning'].update(value="",visible=False)
 
             current_todos = functions.get_todos()
-            print("current-todos-----",current_todos)
-
 
             new_todo = values['todo-input'] + "\n"
-            print("new todo---", new_todo)
-
-
-
             current_todos.append(new_todo)
 
-
             functions.write_todos(current_todos)
+
             window['todos-box'].update(values=current_todos)
             window['todo-input'].update(value="")
 
@@ -55,29 +59,42 @@ while True:
             window['todo-input'].update(value=selected_todo)
 
         case 'Edit':
+            try:
+                todo_to_edit = values['todos-box'][0].replace("\n","")
+                current_todos = functions.get_todos()
+                current_todos = functions.remove_new_lines(current_todos)
+                index = current_todos.index(todo_to_edit)
 
-            if values['todo-input'] == "":
-                window['warning'].update(value="cannot edit empty todo",visible=True)
-                continue
-            window['warning'].update(value="",visible=False)
+                new_todo = values['todo-input'].replace("\n","")
+
+                current_todos[index] = new_todo
+
+                current_todos = functions.add_new_line(current_todos)
+
+                functions.write_todos(current_todos)
+                window['todos-box'].update(values=current_todos)
+                window['todo-input'].update(value="")
+
+            except IndexError,ValueError :
+                fsg.popup("Please select a todo first",title="Error")
+
+        case "Complete":
+            try:
+                todo_to_complete = values['todos-box'][0].replace("\n", "")
+                all_todos = functions.get_todos()
+                all_todos = functions.remove_new_lines(all_todos)
+                all_todos.remove(todo_to_complete)
+
+                all_todos = functions.add_new_line(all_todos)
+
+                functions.write_todos(all_todos)
+
+                window['todos-box'].update(values=all_todos)
+                window['todo-input'].update(value="")
 
 
-
-            todo_to_edit = values['todos-box'][0].replace("\n","")
-            current_todos = functions.get_todos()
-            current_todos = functions.remove_new_lines(current_todos)
-            index = current_todos.index(todo_to_edit)
-
-            new_todo = values['todo-input']
-
-            current_todos[index] = new_todo
-
-            current_todos = functions.add_new_line(current_todos)
-
-            functions.write_todos(current_todos)
-            window['todos-box'].update(values=current_todos)
-            window['todo-input'].update(value="")
-
+            except IndexError:
+                fsg.popup("select a todo first", title="Error")
         case fsg.WIN_CLOSED :
             break
 window.close()
