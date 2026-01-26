27bde96c32ba0819bbc891153b1ec3e23f21831f
Nikhil Randhawa <ns9013604867@gmail.com>
2026-01-26 19:47:55 +0530

    add 'links', 'title', 'description' placeholder to the body of the email

--- main.py
+++ main.py
@@ -16,10 +16,19 @@ response = requests.get(url=URL)
 data = response.json()
 articles = data["articles"]
 
-body=""
+body="Subject: Today's top news on TECHNOLOGY" + '\n'
+news_number =1
 for article in articles:
+
       if article["title"] is not None and article["description"] is not None:
-          body = body + article["title"] + '\n' + article["description"] + 2*'\n'
+
+          news_count = f"News-{news_number}" + '\n'
+          title = f"Title- {article["title"]}" + 2*'\n'
+          description = "Description- " + article["description"]+ '\n'
+          link = "Read full article: " + article["url"] + 3*'\n'
+
+          body =  body  + news_count+ title + description + link
+          news_number+=1
 
 body = body.encode('utf-8')
 send_email(message=body)
