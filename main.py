6c9a1d6ece62c9c3f3a4acd143879a7380a05653
Nikhil Randhawa <ns9013604867@gmail.com>
2026-01-28 11:42:38 +0530

    add history context aware to the model with 'AgentState'

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from langchain_core.agents import AgentActionMessageLog
 
 from langchain_core.messages import  HumanMessage,AIMessage
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.tools import tool
-from langchain.agents import create_agent
+from langchain.agents import create_agent,AgentState
 from todoist_api_python.api import TodoistAPI
 
 load_dotenv()
@@ -18,13 +18,13 @@ TODOIST_API_KEY=os.getenv('TODOIST_API')
 todoist = TodoistAPI(TODOIST_API_KEY)
 
 @tool
-def add_task(task:str, desc:str=None)-> None:
+def add_task(task:str, desc:str|None=None)-> None:
     """add task to the list, if user ask to add task something"""
     todoist.add_task(content=task,description=desc)
 
 tools = [add_task]
 model = ChatGoogleGenerativeAI(
-    model='gemini-3-flash-preview',
+    model='gemini-2.5-flash',
     google_api_key=GEMINI_API_KEY,
     temperature=1
 )
@@ -33,20 +33,26 @@ You are a helpful assistance, You will help the user to add task.
 if user want to add todo or task use add_task tool
 """
 
-user_input = "add a task for buy milk with description only amul"
-
 prompt = ChatPromptTemplate([
-     ("system",system_prompt)
-    ,("user", user_input)])
+     ("system",system_prompt),
+     MessagesPlaceholder("history"),
+     ("user", "{input}")
+])
 
 # chain = prompt | model | StrOutputParser()
-
+parser = StrOutputParser()
 agent = create_agent(model=model, tools=tools)
 
-
-response = agent.invoke(
-    {"messages": [{"role": "user", "content": user_input}]}
+state =  AgentState(
+    messages=[]
 )
 
-print(response['messages'])
+while True:
+    user_input = input("You: ")
+    state['messages'].append(HumanMessage(content=user_input))
 
+    state = agent.invoke(state)
+
+    ai_response = state['messages'][-1]
+
+    print("AI:",ai_response.content)
