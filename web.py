553a40a7c5351cebde44c9f3410365ad4611f45c
Nikhil Randhawa <ns9013604867@gmail.com>
2026-01-29 09:46:45 +0530

    add "try/except" handling for submit_chat, add functionality to responding the chat, add avatar for ai

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from gradio.themes.builder_app import themes, sizes
+import getpass
 
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
@@ -8,13 +8,18 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 import gradio as gr
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAIError
 
 load_dotenv()
 
-gemini_key = os.getenv('GEMINI_API_KEY')
+gemini_key = os.getenv('GOOGLE_API_KEY')
+
+
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
 
 model = ChatGoogleGenerativeAI(
-    model='gemini-2.5-flash-lite',
+    model='gemini-3-flash-preview',
     google_api_key=gemini_key,
     temperature=0.7
 )
@@ -26,8 +31,6 @@ example, also you are bit numerous, reply with that tone,
 answer each query min 1 line or max 4-5 lines not beyond that keep it short
 """
 
-history=[]
-
 #creating prompt
 prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt),
@@ -38,11 +41,30 @@ prompt = ChatPromptTemplate.from_messages([
 #creating chain
 chain = prompt | model | StrOutputParser()
 
-page = gr.Blocks(
-    title="Chat with Einstein",
-    theme=gr.themes.Glass(font=[gr.themes.GoogleFont("Inconsolata"), "Arial", "sans-serif"],text_size='lg'),
+def submit_chat(user_mes,hist):
+    try:
+        langchain_history=[]
+        for item in hist:
+            if item['role'] == 'user':
+                langchain_history.append(HumanMessage(content=item['content']))
+            elif item['role'] == 'assistant':
+                langchain_history.append(AIMessage(content=item['content']))
 
-)
+        response = chain.invoke({"input": user_mes, "history":langchain_history})
+
+        return "",hist + [{"role": "user", "content": user_mes},
+                          {"role": "assistant", "content": response}]
+    except ChatGoogleGenerativeAIError as e:
+        return  "", [{"role": "user", "content":user_mes},
+            {"role": "assistant",
+                    "content": "⚠️Sorry, Einstein reached his limit today"}]
+
+    except Exception as e:
+        return "", [{"role": "user", "content":user_mes},
+            {"role": "assistant",
+                    "content": f"unexpected error: {str(e)}"}]
+
+page = gr.Blocks(title="Chat with Einstein")
 
 with page:
     gr.Markdown(
@@ -51,9 +73,14 @@ with page:
         Welcome to personal conversation with Albert Einstein!
         """
     )
-    chatbox = gr.Chatbot()
+    chatbox = gr.Chatbot(show_label=False,avatar_images=(None,"Einstein.jpg"))
 
-    msg= gr.Textbox()
+    msg= gr.Textbox(placeholder="Ask me anything", autofocus=True,show_label=False)
 
-    clear = gr.Button()
-page.launch(share=True)
+    msg.submit(submit_chat, inputs=[msg,chatbox], outputs=[msg,chatbox])
+
+    clear = gr.Button(value='Clear', variant='primary',)
+
+page.launch(share=True,
+            theme=gr.themes.Glass(font=[gr.themes.GoogleFont("Inconsolata"),
+            "Arial", "sans-serif"],text_size='lg'),)
