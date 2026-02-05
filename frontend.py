d13bc456752a864e9baf3a025243a9641e0bb583
Nikhil Randhawa <ns9013604867@gmail.com>
2026-02-05 20:01:58 +0530

    add webcam to detect moving objects and rectangle around it

--- frontend.py
+++ frontend.py
@@ -4,9 +4,35 @@ import time
 video = cv2.VideoCapture(1)
 time.sleep(1.5)
 
+first_frame = None
 while True:
     check, frame = video.read()
-    cv2.imshow("My video", frame)
+    # gray scaling
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gauss = cv2.GaussianBlur(gray_frame, (21,21), 0)
+
+    # captured the first frame
+    if first_frame is None:
+        first_frame = gray_frame_gauss
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gauss)
+
+    thres_frame = cv2.threshold(delta_frame, 80, 255,
+                                cv2.THRESH_BINARY)[1]
+
+    dil_frame = cv2.dilate(thres_frame, None, iterations=2)
+
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL,
+                                       cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv2.imshow('Video', frame)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
