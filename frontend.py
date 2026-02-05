38a9c9733741f3dd77f7dacb4b9b475be62d3ca7
Nikhil Randhawa <ns9013604867@gmail.com>
2026-02-05 22:55:48 +0530

    add trigger action when the webcam detects ana object

--- frontend.py
+++ frontend.py
@@ -1,11 +1,15 @@
 import cv2
 import time
+from send_email import send_email
 
 video = cv2.VideoCapture(1)
 time.sleep(1.5)
 
+
 first_frame = None
+status_arr = []
 while True:
+    status = 0
     check, frame = video.read()
     # gray scaling
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -17,7 +21,7 @@ while True:
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gauss)
 
-    thres_frame = cv2.threshold(delta_frame, 80, 255,
+    thres_frame = cv2.threshold(delta_frame, 50, 255,
                                 cv2.THRESH_BINARY)[1]
 
     dil_frame = cv2.dilate(thres_frame, None, iterations=2)
@@ -30,7 +34,18 @@ while True:
             continue
 
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+        if rectangle.any():
+            status = 1
+
+    status_arr.append(status)
+    status_arr = status_arr[-2:]
+
+    if len(status_arr) > 1 :
+        if status_arr[0] == 1 and status_arr[1] == 0:
+            send_email()
+        print(status_arr)
 
     cv2.imshow('Video', frame)
 
