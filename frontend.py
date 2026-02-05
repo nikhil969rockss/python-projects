84e119a4dcfa20671b3fd0dc3c73aa4c598cb599
Nikhil Randhawa <ns9013604867@gmail.com>
2026-02-06 00:44:21 +0530

    add extract images from webcam video

--- frontend.py
+++ frontend.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import glob
 from send_email import send_email
 
 video = cv2.VideoCapture(1)
@@ -8,6 +9,7 @@ time.sleep(1.5)
 
 first_frame = None
 status_arr = []
+count = 1
 while True:
     status = 0
     check, frame = video.read()
@@ -38,6 +40,14 @@ while True:
 
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*png")
+            index = int(len(all_images) / 2) - int((len(all_images) / 2)/2)
+            print("index-",index)
+            image_with_object = all_images[index]
+
+
 
     status_arr.append(status)
     status_arr = status_arr[-2:]
@@ -45,7 +55,6 @@ while True:
     if len(status_arr) > 1 :
         if status_arr[0] == 1 and status_arr[1] == 0:
             send_email()
-        print(status_arr)
 
     cv2.imshow('Video', frame)
 
