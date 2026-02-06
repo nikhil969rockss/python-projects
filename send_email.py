6246319d32011da8b8c7412b8a93e3016b43b8f1
Nikhil Randhawa <ns9013604867@gmail.com>
2026-02-06 11:38:32 +0530

    add 'send email' function with smtp lib, but testing failed because of gmail verification issues

--- send_email.py
+++ send_email.py
@@ -1,2 +1,32 @@
-def send_email():
-    print("email was sent")
+import smtplib
+from email.message import EmailMessage
+import imghdr
+
+PASSWORD = "<Your app password>"
+sender_email= "<your email id>"
+receiver_email= "<receiver email id>"
+
+def send_email(image_file):
+    email_message = EmailMessage()
+    email_message["Subject"] = "A new person is detected"
+    email_message.set_content("check the person's photo")
+
+    with open(image_file, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(content, maintype="image",
+                                 subtype=imghdr.what(None, content))
+
+    gmail  = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(sender_email, PASSWORD)
+    gmail.sendmail(sender_email, receiver_email, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ = '__main__':
+    send_email("images/20.png")
+
+# This i can't test because Gmail has banned the email id which was 2 step
+# verified, because it detects that it was created by a computer program.
