2c42b00a56efe19c8cad8cb19daaaee310bf546a
Nikhil Randhawa <ns9013604867@gmail.com>
2026-02-20 22:49:10 +0530

    refactor: OOPs concept of classes

--- main.py
+++ main.py
@@ -4,28 +4,46 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+class WebAutomation():
+    def __init__(self, URL) :
+        self.URL = URL
 
-URL="https://the-internet.herokuapp.com/login"
+        # define options, webdriver
+        options = Options()
+        options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
+        options.add_argument("--disable-search-engine-choice-screen")
+        self.driver = webdriver.Chrome(options=options) # type: ignore
 
-# define options, webdriver
-options = Options()
-options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
-options.add_argument("--disable-search-engine-choice-screen")
-driver = webdriver.Chrome(options=options)
+    def load_page(self):
+        self.driver.get(self.URL)
+    
+    def locate_by_Id(self, id):
+        element = self.driver.find_element(By.ID,id)
+        return element
+    
+    def locate_by_XPath(self, XPath):
+        element = self.driver.find_element(By.XPATH,XPath)
+        return element
+    
+    def execute(self, button):
+        self.driver.execute_script("arguments[0].click();", button)
 
-#Load the web page
-driver.get(URL)
+    def login_user(self, username, password):
+        username_field = self.locate_by_Id("username")
+        password_field = self.locate_by_Id("password")
+        login_button = self.locate_by_XPath('//*[@id="login"]/button')
 
-username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,"username")))
-password_field = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.ID,"password")))
-login_button = driver.find_element(By.XPATH, '//*[@id="login"]/button')
-print(login_button)
+        #fill the form
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+        self.execute(login_button)
+    
+    def exit(self):
+        input("Press enter to quit browser ")
+        self.driver.quit()
 
-#fill the fields
-username_field.send_keys("tomsmith")
-password_field.send_keys("SuperSecretPassword!")
-#execute the button us js script
-driver.execute_script("arguments[0].click();",login_button)
 
-input("Press enter to quit browser ")
-driver.quit()
+automate = WebAutomation('https://the-internet.herokuapp.com/login')
+automate.load_page()
+automate.login_user(username='tomsmith', password='SuperSecretPassword!')
+automate.exit()
