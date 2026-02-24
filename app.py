c2602e7a4ac60f5b4289396082ebd4b8cd03337c
Nikhil Randhawa <ns9013604867@gmail.com>
2026-02-24 05:55:25 +0530

    getting the data from the frontend

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET', 'POST'])
 def home_page():
+    if request.method =='POST':
+       first_name = request.form['first_name']
+       last_name = request.form['last_name']
+       email = request.form['email']
+       date = request.form['date']
+       occupation = request.form['occupation']
+
+       print(occupation, first_name)
     return render_template("index.html")
 
 
